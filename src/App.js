3226f8b93780d1919fa39b758406fbadfeba8c1a
BaielS <bsatylganov25@gmail.com>
2023-09-17 15:56:56 +0600

    инишл коммит =)

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Container from "@mui/material/Container";
+import { Route, Routes } from "react-router-dom";
+
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login, Footer } from "./pages";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </Container>
+      {/* <Footer/> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+import Container from "@mui/material/Container";
+
+export const Header = () => {
+  const isAuth = false;
+  const onClickLogout = () => console.log("hello");
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>ARCHAKOV BLOG</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/posts/create">
+                  <Button variant="contained">Написать статью</Button>
+                </Link>
+                <Button onClick={onClickLogout} >Выйти</Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
